025fb8f7d1733be64bab387ead1013384cc6d619
Ahmad Saadat <ahmad.saadat.official@gmail.com>
2023-08-02 15:18:08 -0400

    feature: finished tests

--- src/main.py
+++ src/main.py
@@ -51,7 +51,69 @@ def calculate_groupby_constituents_2(df):
     groupby_constituent_df = groupby_constituent_df.reset_index(inplace=False)
     return groupby_constituent_df
 
+def calculate_compare_days_difference_3(df):
+# Compare DAY1 to DAY2.  For a given ETF, indicate which constituent has dropped from DAY1 to 
+# DAY2, and which constituent has been added from DAY1 to DAY2
+
+    day1_dataframe = df[df['Day'] == 1]
+    # day1_dataframe = day1_dataframe[day1_dataframe['Composite AxiomaID'] == 'ETF4']
+    day2_dataframe = df[df['Day'] == 2]
+    # day2_dataframe = day2_dataframe[day2_dataframe['Composite AxiomaID'] == 'ETF4']
     
+    # do a left join to find out what rows are only in Day1 and not in Day2
+    left_join_dataframes = pd.merge(day1_dataframe, day2_dataframe, how='left', on=["Composite AxiomaID", "Constituent AxiomaID"], suffixes=['_day1', '_day2'])
+    # keep only dropped constituents per ETF
+    dropped_dataframes = left_join_dataframes[(left_join_dataframes['Weight_day2'].isnull()) & (left_join_dataframes['Day_day2'].isnull())]
+    dropped_dataframes = dropped_dataframes.drop(['Weight_day2', 'Day_day2'], axis=1)
+    dropped_dataframes = dropped_dataframes.rename(columns={'Weight_day1': 'Weight', 'Day_day1':'Day'})
+    
+    # do a right join to find out what rows are only in Day2 and not in Day1
+    right_join_dataframes = pd.merge(day1_dataframe, day2_dataframe, how='right', on=["Composite AxiomaID", "Constituent AxiomaID"], suffixes=['_day1', '_day2'])
+    added_dataframes = right_join_dataframes[(right_join_dataframes['Weight_day1'].isnull()) & (right_join_dataframes['Day_day1'].isnull())]
+    added_dataframes = added_dataframes.drop(['Weight_day1', 'Day_day1'], axis=1)
+    added_dataframes = added_dataframes.rename(columns={'Weight_day2': 'Weight', 'Day_day2':'Day'})
+    
+    #reset indexes
+    dropped_dataframes = dropped_dataframes.reset_index(inplace=False)
+    added_dataframes = added_dataframes.reset_index(inplace=False)
+    
+    #reposition column indexes
+    reposition_cols = ["Day","Composite AxiomaID","Constituent AxiomaID","Weight"]
+    dropped_dataframes = dropped_dataframes.reindex(columns=reposition_cols)
+    added_dataframes = added_dataframes.reindex(columns=reposition_cols)
+
+    #return a list, the former containing dropped constituents per ETF, and the latter containing added constituents per ETF
+    return [dropped_dataframes, added_dataframes]
+    
+def calculate_percentage_change_per_constituent_4(df):
+    # For each ETF, indicate which constituent’s weight has changed the MOST from DAY1 to DAY2
+
+    day1_dataframe = df[df['Day'] == 1]
+    day1_dataframe = day1_dataframe.rename(columns={'Weight' : 'Weight Day1'})
+    # print(day1_dataframe)
+
+    day2_dataframe = df[df['Day'] == 2]
+    day2_dataframe = day2_dataframe.rename(columns={'Weight' : 'Weight Day2'})
+    # print(day2_dataframe)
+
+    combined_dfs = pd.merge(day1_dataframe, day2_dataframe, on=['Composite AxiomaID','Constituent AxiomaID'], how='inner')
+    combined_dfs = combined_dfs.drop(['Day_x', 'Day_y'], axis=1)
+
+    combined_dfs['pct_change'] = ((combined_dfs['Weight Day2'] - combined_dfs['Weight Day1']) / combined_dfs['Weight Day1']) * 100
+    
+    combined_dfs = combined_dfs.sort_values(by='pct_change', axis=0, inplace=False, ascending=False)
+    
+    # group by and find max pct_change per ETF, return dataframe
+    # combined_dfs = combined_dfs.groupby(['Composite AxiomaID'])['pct_change'].max().to_frame()
+    
+    # instead of doing group by trying getting a series
+    max_index_series = combined_dfs.groupby(['Composite AxiomaID'])['pct_change'].idxmax()
+    combined_dfs = combined_dfs.loc[max_index_series]
+    
+    #reset and recalibrate indexes
+    combined_dfs = combined_dfs.reset_index(inplace=False, drop=True)
+    
+    return combined_dfs
 
 if __name__== "__main__":
     directory = './data/input_raw/'
--- test/test_main.py
+++ test/test_main.py
@@ -16,8 +16,6 @@ def test_calculate_distinct_ETFs_1():
     # check to see if return type of result_df is dataframe
     assert isinstance(result_df, pd.DataFrame)
     
-    print(result_df)
-    
     test_df = pd.DataFrame(
     [[1, 4], 
      [2, 3]],
@@ -39,8 +37,6 @@ def test_calculate_groupby_constituents_2():
     # check to see if return type of result_df is dataframe
     assert isinstance(result_df, pd.DataFrame)
     
-    print(result_df)
-    
     test_df = pd.DataFrame(
     [
         [1, 'ETF1', 4], 
@@ -59,8 +55,82 @@ def test_calculate_groupby_constituents_2():
     # check to see if the calculation was correct
     pd.testing.assert_frame_equal(result_df, test_df)
     
+def test_calculate_compare_days_difference_3():
+# Compare DAY1 to DAY2. For a given ETF, indicate which constituent has dropped from DAY1 to
+# DAY2, and which constituent has been added from DAY1 to DAY2
+
+    directory = './data/input_raw/'
+    build_df = main.build_df(directory)
+    result_df = main.calculate_compare_days_difference_3(build_df)
+    dropped_df = result_df[0]
+    added_df = result_df[1]
+    
+    # check to see if return type of build_df is dataframe
+    assert isinstance(build_df, pd.DataFrame)
+    # check to see if return type of result_df is list
+    assert isinstance(result_df, list)
+    # check to see if contents are of type DataFrame
+    assert isinstance(dropped_df, pd.DataFrame)
+    assert isinstance(added_df, pd.DataFrame)
+    
+    test_dropped_df = pd.DataFrame(
+    [
+        [1, 'ETF1', 'MSFT',  0.1250],
+        [1, 'ETF2', 'AAPL',  0.3333],
+        [1, 'ETF3', 'MA',  0.8000],
+        [1, 'ETF4', 'TUP',  0.1000],
+        [1, 'ETF4', 'NVDA',  0.1000],
+        [1, 'ETF4', 'MA',  0.8000]
+    ],
+    columns=['Day', 'Composite AxiomaID', 'Constituent AxiomaID', 'Weight']
+    )
+    
+    test_added_df = pd.DataFrame(
+    [
+        [2, 'ETF2', 'TSLA',  0.4],
+        [2, 'ETF3', 'HD',  0.8]
+    ],
+    columns=['Day', 'Composite AxiomaID', 'Constituent AxiomaID', 'Weight']
+    )
+    
+    # check to see if the dropped_df is 6x2
+    assert dropped_df.shape == (6, 4)
+    # check to see if the dropped constituents was correct
+    pd.testing.assert_frame_equal(dropped_df, test_dropped_df)
+    # check to see if the added_df is 6x2
+    assert added_df.shape == (2, 4)
+    # check to see if the added constituents was correct
+    pd.testing.assert_frame_equal(added_df, test_added_df)
+
+def test_calculate_percentage_change_per_constituent_4():
+    # For each ETF, indicate which constituent’s weight has changed the MOST from DAY1 to DAY2
+    directory = './data/input_raw/'
+    build_df = main.build_df(directory)
+    result_df = main.calculate_percentage_change_per_constituent_4(build_df)
+    
+    test_df = pd.DataFrame(
+    [
+        ['ETF1', 'TSLA', 0.2500, 0.30, 20.000000],
+        ['ETF2', 'MSFT',  0.3333, 0.40, 20.012001],
+        ['ETF3', 'V',  0.1000, 0.15, 50.000000]
+    ],
+    columns=['Composite AxiomaID', 'Constituent AxiomaID', 'Weight Day1', 'Weight Day2', 'pct_change']
+    )
+    
+    # check to see if return type of build_df is dataframe
+    assert isinstance(build_df, pd.DataFrame)
+    # check to see if return type of result_df is list
+    assert isinstance(result_df, pd.DataFrame)
+    
+    # check to see if the added_df is 3x5
+    assert result_df.shape == (3, 5)
+    # check to see if percentage change per ETF and constituents is correct
+    pd.testing.assert_frame_equal(result_df, test_df)
+
     
     
 if __name__== "__main__":
     test_calculate_distinct_ETFs_1()
     test_calculate_groupby_constituents_2()
+    test_calculate_compare_days_difference_3()
+    test_calculate_percentage_change_per_constituent_4()
